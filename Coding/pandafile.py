1c190a5f5e0ec99d7ad0770883abe5d43968f954
techsabby <ksabby89@yahoo.com>
2021-07-08 15:41:53 +0800

    folder removed

--- Coding/pandafile.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-# df1 = pandas.DataFrame([[2,4,6], [10,20,30]], columns=["Price", "Age", "Value"], index=["First", "Second"])
-
-# print(df1)
-# print(type(df1))
-import pandas
-df1 = pandas.read_json("C:/Users/Peter/Desktop/supermarkets/supermarkets.json")
-print(df1)
